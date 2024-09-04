ebc63c5972b170ba2273f5ef9bfbbd53709c020c
FERNANDA CAROLINA BATISTA DE SOUZA <fernanda.c.souza7@aluno.senai.br>
2024-09-04 12:11:01 -0300

    first commit

---
+++ routers/dependencias.py
@@ -0,0 +1,38 @@
+from fastapi import APIRouter
+from models.dependencia import DependenciaDB, UnidadeConsumidoraDB
+from schemas.dependencia import DependenciaCreate, DependenciaReadList, DependenciaReadOne, DependenciaUpdate
+
+router = APIRouter(prefix='/dependencias', tags=['DEPENDÊNCIAS'])
+
+@router.post('', response_model=DependenciaReadOne)
+def criar_dependencia(novo_dependencia: DependenciaCreate):
+    dependencia = DependenciaDB.create(**novo_dependencia.model_dump())
+    return dependencia
+
+@router.get('/unidade-consumidora/{unidade_consumidora_id}', response_model=DependenciaReadList)
+def listar_dependencias_da_unidade_consumidora(unidade_consumidora_id: int):
+    unidade_consumidora = UnidadeConsumidoraDB.get_or_none(UnidadeConsumidoraDB.id == unidade_consumidora_id)
+    if unidade_consumidora:
+        dependencias = DependenciaDB.select().where(DependenciaDB.unidade_consumidora == unidade_consumidora)
+        return {'dependencias': dependencias}
+    return {'dependencias': []}
+
+@router.get('/{dependencia_id}', response_model=DependenciaReadOne)
+def listar_dependencia(dependencia_id: int):
+    dependencia = DependenciaDB.get_or_none(DependenciaDB.id == dependencia_id)
+    return dependencia
+
+@router.patch('/{dependencia_id}', response_model=DependenciaReadOne)
+def atualizar_dependencia(dependencia_id: int, novo_dependencia: DependenciaUpdate):
+    dependencia = DependenciaDB.get_or_none(DependenciaDB.id == dependencia_id)
+    if dependencia:
+        dependencia.nome = novo_dependencia.nome
+        dependencia.save()
+    return dependencia
+
+@router.delete('/{dependencia_id}', response_model=DependenciaReadOne)
+def excluir_dependencia(dependencia_id: int):
+    dependencia = DependenciaDB.get_or_none(DependenciaDB.id == dependencia_id)
+    if dependencia:
+        dependencia.delete_instance()
+    return dependencia
---
+++ routers/dispositivos.py
@@ -0,0 +1,51 @@
+from fastapi import APIRouter
+from models.dispositivo import DispositivoDB
+from models.unidade_consumidora import UnidadeConsumidoraDB
+from models.dependencia import DependenciaDB
+from schemas.dispositivo import DispositivoCreate, DispositivoRead, DispositivoReadList, DispositivoUpdate
+
+router = APIRouter(prefix='/dispositivos', tags=['DISPOSITIVOS'])
+
+@router.post('', response_model=DispositivoRead)
+def criar_dispositivo(novo_dispositivo: DispositivoCreate):
+    dispositivo = DispositivoDB.create(**novo_dispositivo.model_dump())
+    return dispositivo
+
+@router.get('/unidades-consumidoras/{unidade_consumidora_id}', response_model=DispositivoReadList)
+def listar_dispositivos_por_unidade_consumidora(unidade_consumidora_id: int):
+    unidade_consumidora = UnidadeConsumidoraDB.get_or_none(UnidadeConsumidoraDB.id == unidade_consumidora_id)
+    if unidade_consumidora:
+        dispositivos = DispositivoDB.select().where(DispositivoDB.unidade_consumidora == unidade_consumidora)
+        return {'dispositivos': dispositivos}
+    return {'dispositivos': []}
+
+@router.get('/dependencias/{dependencia_id}', response_model=DispositivoReadList)
+def listar_dispositivos_por_dependencia(dependencia_id: int):
+    dependencia = DependenciaDB.get_or_none(DependenciaDB.id == dependencia_id)
+    if dependencia:
+        dispositivos = DispositivoDB.select().where(DispositivoDB.dependencia == dependencia)
+        return {'dispositivos': dispositivos}
+    return {'dispositivos': []}
+
+@router.get('/{dispositivo_id}', response_model=DispositivoRead)
+def listar_dispositivo(dispositivo_id: int):
+    dispositivo = DispositivoDB.get_or_none(DispositivoDB.id == dispositivo_id)
+    return dispositivo
+
+@router.patch('/{dispositivo_id}', response_model=DispositivoRead)
+def atualizar_dispositivo(dispositivo_id: int, dispositivo_atualizado: DispositivoUpdate):
+    dispositivo = DispositivoDB.get_or_none(DispositivoDB.id == dispositivo_id)
+    if dispositivo:
+        dispositivo.nome = dispositivo_atualizado.nome
+        dispositivo.consumo = dispositivo_atualizado.consumo
+        dispositivo.uso_diario = dispositivo_atualizado.uso_diario
+        dispositivo.tipo_id = dispositivo_atualizado.tipo_id
+        dispositivo.save()
+    return dispositivo
+
+@router.delete('/{dispositivo_id}', response_model=DispositivoRead)
+def eliminar_dispositivo(dispositivo_id: int):
+    dispositivo = DispositivoDB.get_or_none(DispositivoDB.id == dispositivo_id)
+    if dispositivo:
+        dispositivo.delete_instance()
+    return dispositivo
